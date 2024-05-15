05dd510f10256672bb0f875ee5157d1073f51586
Edward @ Hetzner <edward@tanguay.info>
2024-05-15 06:56:00 +0000

    works with modules

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-const fs = require("fs").promises;
-const path = require("path");
+import {promises as fs} from 'fs';
+import path from 'path';
 
 const dataPath = path.join(process.env.DATA_PATH || "./data.txt");
 
-fs.readFile(dataPath)
-  .then(buffer => {
-		    const data = buffer.toString();
-				const newCount = +data + 1;
-		    console.log(newCount);
-		    writeTo(newCount);
-		  })
-  .catch(e => {
-		    console.log("file not found, writing '0' to a new file");
-		    writeTo(0);
-		  });
+(async()=>{
+	try {
+		const buffer = await fs.readFile(dataPath);
+		const data = buffer.toString();
+		const newCount = +data + 1;
+		console.log(newCount);
+		writeTo(newCount);
+	}
+  catch(e){
+		writeTo(0);
+		console.log("File not found, wrote '0' to a new file.");
+	}
+})();
 
 const writeTo = data => {
 	  fs.writeFile(dataPath, data.toString()).catch(console.error);
